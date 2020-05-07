5a3776a56edd34868d926ae8c1a85b73bb1f1fe1
M6code <benjamynfrank101@gmail.com>
2020-05-07 15:27:17 +0100

    Updates

---
+++ precache-manifest.e83c560a5e72bce7087cfa4c6fd46059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c65ecf4b06b44c8a73979c04ce4c71",
+    "url": "/todo-app-demo/index.html"
+  },
+  {
+    "revision": "bdb555faeae5973956db",
+    "url": "/todo-app-demo/static/css/main.39a6b2c8.chunk.css"
+  },
+  {
+    "revision": "22a9f540a0e05519444c",
+    "url": "/todo-app-demo/static/js/2.1e32d2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-demo/static/js/2.1e32d2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb555faeae5973956db",
+    "url": "/todo-app-demo/static/js/main.9ad70f60.chunk.js"
+  },
+  {
+    "revision": "33bf76abd7c6b8e4bbb8",
+    "url": "/todo-app-demo/static/js/runtime-main.2081b401.js"
+  }
+]);
